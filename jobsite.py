2572978e03575457c510db90422f404870575f1f
fahmed3 <fahmed3@stuy.edu>
2017-09-26 20:08:24 -0400

    Finished with Kristin

--- jobsite.py
+++ jobsite.py
@@ -1,9 +1,14 @@
+#Fabiha Ahmed, Kristin Lin
+#SoftDev Pd 9
+#Work05 - Occupational Jinja Training
+#2017-09-27
 
 from flask import Flask, render_template
 import occupations
 
 jobsite = Flask(__name__)
 
+@jobsite.route('/')
 @jobsite.route('/occupations')
 def occupy() :
     return render_template('jobs.html',
